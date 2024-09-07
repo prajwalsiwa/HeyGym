1e92993ac07ea02a98bbc9efe9fda0180064d523
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-07 08:48:39 +0545

    feat: create label component

---
+++ src/Components/common/FormUi/Label.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+interface ILabelProps {
+  children?: ReactNode;
+  htmlFor: string | number;
+  required?: boolean;
+}
+
+export default function Label({ children, htmlFor, required }: ILabelProps) {
+  return (
+    <label
+      className="text-sm text-grey-800"
+      htmlFor={htmlFor?.toString()}
+    >
+      {children}
+      {required && <span className="text-red-500">*</span>}
+    </label>
+  );
+}
