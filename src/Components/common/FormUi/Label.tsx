053b3e52c3cd3333de11c732e51244ff1c630a2b
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-08 08:57:15 +0545

    refactor: add classname

--- src/Components/common/FormUi/Label.tsx
+++ src/Components/common/FormUi/Label.tsx
@@ -4,12 +4,13 @@ interface ILabelProps {
   children?: ReactNode;
   htmlFor: string | number;
   required?: boolean;
+  className?: string;
 }
 
-export default function Label({ children, htmlFor, required }: ILabelProps) {
+export default function Label({ children, htmlFor, required, className }: ILabelProps) {
   return (
     <label
-      className="text-sm text-grey-800"
+      className={`text-sm text-grey-800 ${className}`}
       htmlFor={htmlFor?.toString()}
     >
       {children}
