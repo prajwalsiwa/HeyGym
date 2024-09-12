6cd20cc77de4c15cda8dc46a468d90aea13d2117
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-12 08:32:31 +0545

    feat: create toggle switch component

---
+++ src/Components/Report/ToggleSwich.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../common/Button";
+
+function ToggleSwitch() {
+  return (
+    <div className="rounded-3xl p-2">
+      <Button />
+    </div>
+  );
+}
+
+export default ToggleSwitch;
