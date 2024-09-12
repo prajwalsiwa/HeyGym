0f8382edd24b82a18113b72bf04501395756bcdb
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-12 08:32:13 +0545

    feat: create statistics component

---
+++ src/Components/Report/Statistics.tsx
@@ -0,0 +1,11 @@
+import ToggleSwitch from "./ToggleSwich";
+
+function Statistics() {
+  return (
+    <div className="h-[74%]  bottom-0 rounded-t-3xl absolute w-full border-box bg-white">
+      <ToggleSwitch />
+    </div>
+  );
+}
+
+export default Statistics;
