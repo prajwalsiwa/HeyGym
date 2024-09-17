8af30460a8d9782fb421850e82cd3c3139402f53
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-17 22:42:16 +0545

    feat: include report card in dashboard

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,3 +1,42 @@
+import ReportCard from "@/Components/Dashboard/ReportCard";
+import reportCardList from "@/constants/reportCardList";
+
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div className="px-8 bg-slate-100 w-full">
+      {/* upcoming expiry report  */}
+      <div className="">
+        <h5 className="">Upcoming Expiry Report</h5>
+        <div className="grid gap-10 grid-cols-2">
+          {reportCardList.map((report) => (
+            <ReportCard
+              members={report.members}
+              description={report.description}
+            />
+          ))}
+        </div>
+      </div>
+      {/* registration report  */}
+      <div>
+      <h5 className="">Registration Report</h5>
+        <div className="grid gap-10 grid-cols-2">
+          {reportCardList.map((report) => (
+            <ReportCard
+              members={report.members}
+              description={report.description}
+            />
+          ))}
+        </div>
+      </div>
+      {/* payment report  */}
+      <div className="grid gap-10 grid-cols-2">
+        {reportCardList.map((report) => (
+          <ReportCard
+            members={report.members}
+            description={report.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
