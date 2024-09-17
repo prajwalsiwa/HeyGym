9201bad3b5c47715a3b68488548993d1bbb3340c
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-17 22:41:10 +0545

    feat: include date switch

--- src/Components/Report/DateSwitch.tsx
+++ src/Components/Report/DateSwitch.tsx
@@ -2,19 +2,18 @@ import { Button } from "../common/Button";
 import { dateObj } from "./Statistics";
 
 interface dateSwitchProps {
-  date: dateObj[]
+  date: dateObj[];
 }
 
-
-
-function DateSwitch({date}: dateSwitchProps) {
-
-  console.log(date,'this is date')
+function DateSwitch({ date }: dateSwitchProps) {
+  console.log(date, "this is date");
   return (
     <div className="rounded-xl border w-fit h-[3rem] border-rose-400 p-1.5 gap-2 flex justify-between ">
-     {
-      date.map((item)=> <Button className="px-2 flex justify-center items-center rounded-lg  bg-rose-500 text-white">{item.name}</Button>)
-     }
+      {date.map((item) => (
+        <Button className="px-2 flex justify-center items-center rounded-lg  bg-rose-500 text-white">
+          {item.name}
+        </Button>
+      ))}
     </div>
   );
 }
